12b4ac94797529c7c5465b5fc323a86d72c3e5a7
dan <chronos937@gmail.com>
2019-10-10 21:22:06 -0400

    added to the starter code with a start function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,7 +5,7 @@ var wins = 0;
 var losses = 0;
 
 
-function start {
+function start() {
     var button1 = Math.floor(Math.random() * 100) + 1;
     var button2 = Math.floor(Math.random() * 100) + 1;
     var button3 = Math.floor(Math.random() * 100) + 1;
