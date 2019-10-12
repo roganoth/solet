477faba321314f3bc8e8e899a038c74f62f09820
dan demeo <chronos937@gmail.com>
2019-10-12 16:31:48 -0400

    got it all working except when not confirming, calls start anyway.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,90 +1,83 @@
 $(document).ready();
 
-var scoreTotal = [];
-var total = 0
+var total = 0;
 var wins = 0;
 var losses = 0;
 
-var button1 = Math.floor(Math.random() * 50) + 1;
-var button2 = Math.floor(Math.random() * 50) + 1;
-var button3 = Math.floor(Math.random() * 50) + 1;
-var button4 = Math.floor(Math.random() * 50) + 1;
-var goal = Math.floor(Math.random() * 51) + 50;
-console.log(button1);
-console.log(button2);
-console.log(button3);
-console.log(button4);
+var button1 = Math.floor(Math.random() * 10) + 1;
+var button2 = Math.floor(Math.random() * 10) + 1;
+var button3 = Math.floor(Math.random() * 10) + 1;
+var button4 = Math.floor(Math.random() * 10) + 1;
+var goal = Math.floor(Math.random() * 40) + 1;
+// console.log(button1);
+// console.log(button2);
+// console.log(button3);
+// console.log(button4);
 
-function sum() {
-
-    var total = 0;
-    for (var i = 0; i < scoreTotal.length; i++) {
-        if (isNaN(scoreTotal[i])) {
-            continue;
-        }
-        total += Number(scoreTotal[i]);
-    }
+function compare() {
     if (total === goal) {
         alert("Congratulations! You've won!");
         confirm("Would you like to try again?");
+        wins++;
         if (confirm) {
             start();
         }
-        wins++;
+        else {
+            // $("#wins").text("Wins: " + wins);
+            // return false;
+        }
     }
     if (total > goal) {
         alert("Sorry, you've lost.");
         confirm("Would you like to try again?");
-        scoreTotal=[]
-        console.log(scoreTotal)
-        $("#actualScore").text(scoreTotal);
         losses++;
         if (confirm) {
             start();
         }
-        console.log(losses)
+        else {
+            // $("#losses").text("Losses: " + losses);
+            // return false;
+        }
     }
-    return total;
-
-
 }
 
-
 $("#goal").text(goal);
-$("#wins").append(wins);
-$("#losses").append(losses);
-$("#actualScore").text(sum);
+$("#wins").text("Wins: " + wins);
+$("#losses").text("Losses: " + losses);
+$("#actualScore").text(total);
 
 function start() {
-    button1 = Math.floor(Math.random() * 50) + 1;
-    button2 = Math.floor(Math.random() * 50) + 1;
-    button3 = Math.floor(Math.random() * 50) + 1;
-    button4 = Math.floor(Math.random() * 50) + 1;
-    goal = Math.floor(Math.random() * 51) + 50;
+    button1 = Math.floor(Math.random() * 10) + 1;
+    button2 = Math.floor(Math.random() * 10) + 1;
+    button3 = Math.floor(Math.random() * 10) + 1;
+    button4 = Math.floor(Math.random() * 10) + 1;
+    goal = Math.floor(Math.random() * 40) + 1;
     $("#goal").text(goal);
-    $("#wins").append(wins);
-    $("#losses").append(losses);
-    scoreTotal =[]
-    }
+    $("#losses").text("Losses: " + losses);
+    total = 0;
+    $("#actualScore").text(total)
+}
 
 $("#gemButton1").click(function () {
-    console.log(button1);
-    scoreTotal.push(button1);
-    console.log(scoreTotal);
+    total = total + button1;
+    $("#actualScore").text(total);
+    compare();
 
-    $("#actualScore").text(sum);
 });
 $("#gemButton2").click(function () {
-    scoreTotal.push(button2);
-    $("#actualScore").text(sum);
+    total = total + button2
+    $("#actualScore").text(total);
+    compare();
 });
 $("#gemButton3").click(function () {
-    scoreTotal.push(button3);
-    $("#actualScore").text(sum);
+    total = total + button3
+    $("#actualScore").text(total);
+    compare();
 });
 $("#gemButton4").click(function () {
-    scoreTotal.push(button4);
-    $("#actualScore").text(sum);
+    total = total + button4
+    $("#actualScore").text(total);
+    compare();
 });
 // console.log(total)
 // if (total === goal) {
